7ffdee1f5cd3eb3d411a3c2f42acb80d797ee337
Shreya Prasad <shreyateeza@gmail.com>
2025-01-01 16:00:11 +0530

    Create DFSofCyclicGraph.cpp

---
+++ cpp/graphAlgorithms/DFSofCyclicGraph.cpp
@@ -0,0 +1,20 @@
+void dfsUtil(int root, vector<vector<int>>& adjList, vector<int>& traversal, vector<bool>& visited){
+	visited[root] = true;
+	traversal.push_back(root);
+	for(int i=0; i<adjList[root].size(); i++){
+		int node = adjList[root][i];
+		if(visited[node] == false){
+			dfsUtil(node, adjList, traversal, visited);
+		}
+	}
+}
+
+
+vector<int> dfs(vector<vector<int>> &adjList) {
+    // add your logic here
+	int N = adjList.size();
+	vector<int> traversal;
+	vector<bool> visited(N, false);
+	dfsUtil(0, adjList, traversal, visited);
+	return traversal;
+}
